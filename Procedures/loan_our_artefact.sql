45d81948b4b19094d39b72f70f018e358a4e99f8
AdamC <adamcandrak@gmail.com>
2024-04-24 14:36:37 +0200

    updated this

--- Procedures/loan_our_artefact.sql
+++ Procedures/loan_our_artefact.sql
@@ -19,6 +19,19 @@ BEGIN
         RAISE EXCEPTION 'Artefact is not our or ID is invalid';
     END IF;
 
+    -- check if the our the artefact wont be part of an exposition
+    IF EXISTS(
+        SELECT 1
+        FROM artefact_exhibition
+        JOIN artefacts ON artefacts.id = artefact_exhibition.artefact_id
+        JOIN exhibitions ON exhibitions.id = artefact_exhibition.exhibition_id
+        WHERE artefacts.id = p_artefact_id
+        AND daterange(p_start_date, p_end_date, '[]') && daterange(exhibitions.start_date, exhibitions.end_date, '[]')
+    )
+    THEN
+        RAISE EXCEPTION 'Artefact will part of an exposition at this time';
+    END IF;
+
     INSERT INTO loans(artefact_id, institute_id, expected_arrival_date, start_date, end_date, loan_type)
     VALUES(p_artefact_id, p_institute_id, p_expected_arrival_date, p_start_date, p_end_date, 'loan_out');
 
